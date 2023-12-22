57a4801d33b6b3e9923635d28f170f99d2b69e45
Muhamed Younis <30375527+muhamedyounis@users.noreply.github.com>
2023-12-22 22:55:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+var listOfQuotes = [
+  {
+    content: "“Be yourself; everyone else is already taken.”",
+    author: "― Oscar Wilde",
+  },
+  {
+    content:
+      "“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”",
+    author: "― Marilyn Monroe",
+  },
+  {
+    content:
+      "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”",
+    author: "― Albert Einstein",
+  },
+  {
+    content: "“So many books, so little time.”",
+    author: "― Frank Zappa",
+  },
+  {
+    content: "“A room without books is like a body without a soul.”",
+    author: "― Marcus Tullius Cicero",
+  },
+  {
+    content:
+      "“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”",
+    author: "― Bernard M. Baruch",
+  },
+  {
+    content:
+      "“You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.”",
+    author: "― William W. Purkey",
+  },
+  {
+    content:
+      "“You know you're in love when you can't fall asleep because reality is finally better than your dreams.”",
+    author: "― Dr. Seuss",
+  },
+  {
+    content: "“You only live once, but if you do it right, once is enough.”",
+    author: "― Mae West",
+  },
+  {
+    content: "“Be the change that you wish to see in the world.”",
+    author: "― Mahatma Gandhi",
+  },
+];
+
+function getQuoteBtn() {
+    var randomize = Math.floor(Math.random() * listOfQuotes.length);
+    document.getElementById("quoteContent").innerHTML = listOfQuotes[randomize].content;
+    document.getElementById("quoteAuthor").innerHTML = listOfQuotes[randomize].author;
+}
